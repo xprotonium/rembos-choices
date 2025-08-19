bc5dd25528e0529246424152861315733640ab14
xprotonium <westbrookcricket1@gmail.com>
2025-08-19 18:02:39 +0800

    map layout finalized, texture fixes, player camera and movement tweaks

--- Assets/Scenes/Player/playerMovement.gd
+++ Assets/Scenes/Player/playerMovement.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 50
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
