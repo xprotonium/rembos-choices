14f3d1b20e78774b27f6157ad5c72efc803513e1
Ali Sajulake <westbrookcricket1@gmail.com>
2025-08-26 12:35:22 +0800

    limited the camera bounds and fixed camera process callback to the right selection

--- Assets/Scenes/Operating System/OperatingSystemManager.gd
+++ Assets/Scenes/Operating System/OperatingSystemManager.gd
@@ -3,7 +3,9 @@ extends Control
 @onready var sigmantosh_menu = $SigmantoshMenu
 
 func _on_exit_button_pressed() -> void:
+	# hide the PC UI when the player exits it and also reset the values
 	self.visible = false
+	sigmantosh_menu.visible = false
 
 
 func _on_sigmantosh_button_pressed() -> void:
