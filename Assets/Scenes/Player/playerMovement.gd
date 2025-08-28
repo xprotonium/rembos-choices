49597bfcd972f06f32b3ed4c80134bb8b3690f12
Ali Sajulake <westbrookcricket1@gmail.com>
2025-08-28 13:12:49 +0800

    player now does not move when in UI

--- Assets/Scenes/Player/playerMovement.gd
+++ Assets/Scenes/Player/playerMovement.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 50
+var allow_movement = true
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
 func _physics_process(_delta):
-	get_input()
-	move_and_slide()
+	if allow_movement:
+		get_input()
+		move_and_slide()
