8a52cde187aab52d0946b3253257c4469a942270
Ali Sajulake <westbrookcricket1@gmail.com>
2025-08-28 21:47:33 +0800

    test commit

--- Assets/Scenes/minigame2/background_mananger.gd
+++ Assets/Scenes/minigame2/background_mananger.gd
@@ -15,8 +15,8 @@ func _process(delta: float) -> void:
 
 func _on_player_enter(body: Node):
 	if body is CharacterBody2D:
-		bg1.position.x += 640
+		bg1.position.x += 320 * 2
 
 func _on_player_exit(body: Node):
 	if body is CharacterBody2D:
-		bg2.position.x += 640
+		bg2.position.x += 320 * 2
