7e6df6d720e0db663a034e46a05ce66cb5624ee6
Mohammed Moawia <mm5684267@gmail.com>
2025-09-17 21:30:18 +0800

    QTE system finally done

--- Assets/Scenes/Perfect Timing/QTE.gd
+++ Assets/Scenes/Perfect Timing/QTE.gd
@@ -1,7 +1,4 @@
 extends Container
 
-@export var button : TextureRect
-
-@export var button_ani_player : AnimationPlayer
-func _ready() -> void:
-	button_ani_player.play("QTE")
+@export var button: TextureRect
+@export var button_ani_player: AnimationPlayer
---
+++ Assets/Scenes/Perfect Timing/QTEcode.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+@onready var timer: Timer = $QTETimer
+@onready var e_container = $PerfectTimingUI/ContainerE
+@onready var q_container = $PerfectTimingUI/ContainerQ
+
+var expected_key: String = ""
+var qte_active: bool = false
+
+func _ready():
+	randomize()
+	timer.timeout.connect(_on_qte_failed)
+	start_qte()
+
+func start_qte():
+	if qte_active:
+		return
+
+	var choices = ["qte_e", "qte_q"]
+	expected_key = choices.pick_random()
+
+	e_container.hide()
+	q_container.hide()
+
+	if expected_key == "qte_e":
+		e_container.show()
+		e_container.button_ani_player.play("QTE")
+	else:
+		q_container.show()
+		q_container.button_ani_player.play("QTE")
+
+	print("game start")
+	timer.start()
+	qte_active = true
+
+func _unhandled_input(event: InputEvent):
+	if not qte_active:
+		return
+
+	# Correct key pressed
+	if event.is_action_pressed(expected_key):
+		print("nice")
+		_end_qte()
+	# Wrong QTE key pressed
+	elif event.is_action_pressed("qte_e") or event.is_action_pressed("qte_q"):
+		print("dummy")
+		_end_qte()
+
+func _on_qte_failed():
+	if qte_active:
+		print("time dummy")
+		_end_qte()
+
+func _end_qte():
+	qte_active = false
+	expected_key = ""
+	timer.stop()
+
+	# Hide containers
+	e_container.hide()
+	q_container.hide()
+
+	# Short delay before next QTE
+	await get_tree().create_timer(0.5).timeout
+	
+	start_qte()
