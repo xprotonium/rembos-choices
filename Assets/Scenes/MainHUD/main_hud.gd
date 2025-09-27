9cd90847d5785e69f307092c002fb757154a25b9
Ali Sajulake <westbrookcricket1@gmail.com>
2025-09-27 22:11:38 +0800

    ui fix and added ability to sleep

--- Assets/Scenes/MainHUD/main_hud.gd
+++ Assets/Scenes/MainHUD/main_hud.gd
@@ -2,6 +2,9 @@ extends Control
 
 @onready var death_screen = $DeathScreen
 
+func _ready() -> void:
+	death_screen.visible = false
+
 func _process(delta: float) -> void:
 	if MainGameManager.rembo_dead:
 		death_screen.visible = true
