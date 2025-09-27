c506c5450d73ec5404ae277bcdce98bc5f79c3d8
Ali Sajulake <westbrookcricket1@gmail.com>
2025-09-27 23:25:05 +0800

    demo version release

--- Assets/Scenes/MainHUD/main_hud.gd
+++ Assets/Scenes/MainHUD/main_hud.gd
@@ -1,11 +1,16 @@
 extends Control
 
 @onready var death_screen = $DeathScreen
+@onready var win = $Panel
 
 func _ready() -> void:
+	win.visible = false
 	death_screen.visible = false
 
 func _process(delta: float) -> void:
 	if MainGameManager.rembo_dead:
 		death_screen.visible = true
 		get_tree().paused = true
+	if MainGameManager.you_win == true:
+		win.visible = true
+		get_tree().paused
